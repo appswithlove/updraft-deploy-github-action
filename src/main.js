7ad1bbe8b0e35b22e14594f7f7b30b80c36145bd
Sergey Chuvashev <chuvashev@appswithlove.com>
2024-08-09 18:43:06 +0400

    Fixed curl command

--- src/main.js
+++ src/main.js
@@ -25,9 +25,10 @@ export async function run() {
             '--silent',
             '-f',
             `-F whats_new="${releaseNotes}"`,
-            '-F build_type="bitrise"',
+            '-F build_type="github-action"',
             `-F app=@${appPath}`,
-            `-X PUT https://getupdraft.com/api/app_upload/${appKey}/${apiKey}/`,
+            `-X PUT`,
+            'https://getupdraft.com/api/app_upload/${appKey}/${apiKey}/',
         ]);
         
         console.log(`output: ${commandOutput}, error: ${commandError}`);
